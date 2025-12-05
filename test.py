5164ccd713e158849c0a493330db51a3c6ec339d
theyangcycle <jonahyang2413@gmail.com>
2025-12-04 22:01:16 -0600

    Initial commit

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import sys
-
-input = sys.stdin.readline
-print = sys.stdout.write
-
-x = int(input())
-while x != 1:
-    print(f'{x} ')
-    if x % 2 == 0:
-        x //= 2
+x = list(input())
+x.pop(-2)
+x.pop(-1)
+x = ''.join(x)
+x = int(x)
+if x % 100 in [11,12,13]:
+    print(f'{x}th')
+else:
+    if x % 10 == 1:
+        print(f'{x}st')
+    elif x % 10 == 2:
+        print(f'{x}nd')
+    elif x % 10 == 3:
+        print(f'{x}rd')
     else:
-        x = x*3+1
-    
+        print(f'{x}th')
