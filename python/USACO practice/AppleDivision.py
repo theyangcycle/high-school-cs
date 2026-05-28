b062d2f861949392367d9c3246956fcf2b06cf79
Jonah Yang <jonahyang2413@gmail.com>
2026-05-28 14:32:44 -0500

    Clean up bitwise operators
    Instead of iterating through the binary num, I use i&(1<<j) to check if the jth bit is on in i.

--- python/USACO practice/AppleDivision.py
+++ python/USACO practice/AppleDivision.py
@@ -1,16 +1,13 @@
 n = int(input())
 p = list(map(int,input().split()))
 differences = []
-for i in range(1,2**n):
-    conb = bin(i)
-    group1 = []
-    group2 = []
-    id = 0
-    for j in range(len(conb)-n,len(conb)):
-        if conb[j] == '1':
-            group1.append(p[id])
+for i in range(1<<n):
+    sum1 = 0
+    sum2 = 0
+    for j in range(n):
+        if i & (1<<j):
+            sum1 += p[j]
         else:
-            group2.append(p[id])
-        id+=1
-    differences.append(abs(sum(group1)-sum(group2)))
+            sum2 += p[j]
+    differences.append(abs(sum1-sum2))
 print(min(differences))
